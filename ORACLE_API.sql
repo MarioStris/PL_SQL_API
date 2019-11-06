678d1b44739e19d881a8fe9db620d8891e187b95
Mario Striskovic <>
2019-11-06 19:56:38 +0100

    ADD method input

--- ORACLE_API.sql
+++ ORACLE_API.sql
@@ -6,11 +6,11 @@ TYPE num_obj IS TABLE OF number ;
 procedure CONTROLLER (v_method varchar2, v_json clob);
 
 --dohvat podataka
-procedure POST_METH;
+procedure POST_METH(v_cust num_obj);
 --update podataka
-procedure PUT_METH;
+procedure PUT_METH(v_cust num_obj);
 --delete podataka
-procedure DELETE_METH;
+procedure DELETE_METH(v_cust num_obj);
 
 end;
 /
